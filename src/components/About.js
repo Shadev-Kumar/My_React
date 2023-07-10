7acb0d68f0251927b52078a3a1d7a6bf1d945d01
shadevkumar <shadevsrk261@gmail.com>
2023-07-10 09:20:50 +0530

    initial deploy about

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,14 @@
 import { Component, useEffect } from 'react'
 import User from './User'
 
-class About extends Component {
-  constructor(props) {
-    super(props)
-  }
-  componentDidMount() {
-  }
-  render() {
-    return (
-      <div>
-        <h1>Food Delivery App</h1>
-        <h2>Hi i am Learning React.</h2>
-        <User name={'Shadev Kumar'} location={'Bbsr'} />{' '}
-      </div>
-    )
-  }
+const About = () => {
+  return (
+    <div>
+      <h1>Food Delivery App</h1>
+      <h2>Hi i am Learning React.</h2>
+      <User name={'Shadev Kumar'} location={'Bbsr'} />{' '}
+    </div>
+  )
 }
 
 export default About
